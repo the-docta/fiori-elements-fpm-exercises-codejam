7b42d8d93494bd7b027923031e7e1201cfa11dc3
Sergei Haller <sergei.haller@u-niq.com>
2024-07-10 11:34:47 +0200

    feat: chapter 4: fill custom order section with life

--- bookshop/app/bookshop-ui/webapp/ext/fragment/Order.js
+++ bookshop/app/bookshop-ui/webapp/ext/fragment/Order.js
@@ -1,9 +1,41 @@
-sap.ui.define(["sap/m/MessageToast"], function (MessageToast) {
-    "use strict";
-
+sap.ui.define(["sap/m/MessageToast", "sap/m/Dialog", "sap/m/Button", "sap/m/Text"], function (MessageToast, Dialog, Button, Text) {
     return {
-        onPress: function (oEvent) {
-            MessageToast.show("Custom handler invoked.");
+        onSubmitOrder: function (oEvent) {
+            const oContext = oEvent.getSource().getBindingContext();
+            const oModel = oContext.getModel();
+
+            const selectedBookTitle = oContext.getProperty("title");
+            const selectedBookID = oContext.getProperty("ID");
+            const selectedQuantity = this.byId("stepInput").getValue();
+
+            const oAction = oModel.bindContext("/submitOrder(...)");
+            oAction.setParameter("book", selectedBookID);
+            oAction.setParameter("quantity", selectedQuantity);
+
+            oAction.execute().then(
+                () => {
+                    const sText = `Order successful (${selectedBookTitle}, ${selectedQuantity} pcs.)`;
+                    MessageToast.show(sText);
+                    this.refresh();
+                },
+
+                (oError) => {
+                    this.oErrorMessageDialog = new Dialog({
+                        type: "Standard",
+                        title: "Error",
+                        state: "Error",
+                        content: new Text({ text: oError.error.message }).addStyleClass("sapUiTinyMargin"),
+                        beginButton: new Button({
+                            text: "Close",
+                            press: () => {
+                                this.oErrorMessageDialog.close().destroy();
+                                delete this.oErrorMessageDialog;
+                            }
+                        })
+                    });
+                    this.oErrorMessageDialog.open();
+                }
+            );
         }
     };
 });
